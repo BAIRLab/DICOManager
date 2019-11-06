85b8b1be8924a2d2c8bf169622f06041dad02071
Ron Levitin <rlevitin@gmail.com>
2019-11-06 15:40:36 -0500

    Converted Sequence to Study

--- file_sorting.py
+++ file_sorting.py
@@ -28,8 +28,8 @@ parser.add_option('-c', '--csv', action='store', dest='csv_file',
                   help='MRN csv to sort from', default=None)
 parser.add_option('-d', '--date', action='store_true', dest='date',
                   help='sort modalities by date', default=False)
-parser.add_option('-s', '--sequence', action='store_true', dest='sequence',
-                  help='sequence sorting', default=False)
+parser.add_option('-s', '--study', action='store_true', dest='series',
+                  help='series sorting', default=False)
 
 options, args = parser.parse_args()
 
@@ -75,11 +75,11 @@ def write_to_path(file_path, patientID, dicom_file, data_dir, date=None,
     if subfolder:
         path_list.append(subfolder)
 
-    if options.sequence:  # Specific private tag for Philips MR Scanners
+    if options.series:  # Specific private tag for Philips MR Scanners
         try:
-            # sequence = str(ds[0x2005, 0x140f].value[0][0x0018, 0x9005].value)
-            sequence = str(ds[0x0008, 0x103E].value)
-            path_list.append(sequence)
+            # series = str(ds[0x2005, 0x140f].value[0][0x0018, 0x9005].value)
+            series = str(ds[0x0008, 0x103E].value)
+            path_list.append(series)
         except ValueError:
             pass
 
