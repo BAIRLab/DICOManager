d473072247dd1eccc20097347c506efaed916ca1
Ron Levitin <rlevitin@gmail.com>
2020-01-24 19:23:44 +0000

    Rewritten for sanity. Cut out the date

--- file_sorting.py
+++ file_sorting.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# %%
 import pandas as pd
 import pydicom
 import os
@@ -11,6 +11,7 @@ import optparse
 from tqdm import tqdm
 
 
+# %%
 __author__ = ["Evan Porter", "Ron Levitin", "Nick Myziuk"]
 __liscense__ = "Beaumont Artificial Intelligence Research Lab"
 __email__ = "evan.porter@beaumont.org"
@@ -20,12 +21,16 @@ __status__ = "Research"
 # Alter this path for directories not stored with '/data'
 parser = optparse.OptionParser()
 
-parser.add_option('-a', '--alt', action='store',
-                  dest='alt', help='Alternative source dir', default=None)
+# parser.add_option('-a', '--alt', action='store',
+                #   dest='alt', help='Alternative source dir', default=None)
+parser.add_option('-m', '--move', action='store_true', dest='move_file',
+                  help='Move instead of default of copy', default=False)
 parser.add_option('-b', '--base', action='store', dest='base_dir',
-                  help='Base storing directory', default='pwd')
+                  help='Directory containing unsorted files', default='/data/imported_data/')
 parser.add_option('-c', '--csv', action='store', dest='csv_file',
-                  help='MRN csv to sort from', default=None)
+                  help='MRN csv to sort from, should be located in -b', default=None)
+parser.add_option('-p', '--project-dest', action='store', dest='project_dir',
+                    default = '/data/sorted_data/')
 parser.add_option('-d', '--date', action='store_true', dest='date',
                   help='sort modalities by date', default=False)
 
@@ -34,20 +39,11 @@ options, args = parser.parse_args()
 if not options.csv_file:
     raise NameError('A sorting csv file must be specified with flag --csv')
 
-if options.base_dir == 'pwd':
-    options.base_dir = os.getcwd()
+dicom_files = glob.glob(os.path.join(options.base_dir, '**/*.dcm*'), recursive=True)
 
-if options.alt:
-    dicom_files = glob.glob(os.path.join(options.base_dir,
-                                         options.alt,
-                                         '**/*.dcm'), recursive=True)
-else:
-    dicom_files = glob.glob(os.path.join(options.base_dir,
-                                         'imported_data/*.dcm*'))
+print(f"base_dir: {options.base_dir}\n# of dicoms: {len(dicom_files)}")
 
-
-def _write_to_path(file_path, patientID, dicom_file, data_dir, date=None,
-                   subfolder=False):
+def _write_to_path(dicom_file, dest_dir, patientID, subfolder=False):
     """
     Function
     ---------
@@ -65,10 +61,9 @@ def _write_to_path(file_path, patientID, dicom_file, data_dir, date=None,
     subfolder : bool (Default = False)
         A subfolder to be placed within the file_path
     """
-    path_list = [file_path, patientID]
-
-    if options.date:
-        path_list.append(date)
+    path_list = [dest_dir, patientID]
+    
+    print(dicom_file)
 
     if subfolder:
         path_list.append(subfolder)
@@ -80,14 +75,17 @@ def _write_to_path(file_path, patientID, dicom_file, data_dir, date=None,
 
     _, filename = os.path.split(dicom_file)
     destination = os.path.join(new_path, filename)
-    if options.alt:
-        shutil.copy(os.path.join(data_dir, dicom_file), destination)
+    
+    if options.move_file:
+        count+=1
+        shutil.move(dicom_file, destination)
     else:
-        shutil.move(os.path.join(data_dir, dicom_file), destination)
+        shutil.copy(dicom_file, destination)
 
 
-def _specific_sort(dicom_file, file_path, cohort_list,
-                   patientID, modality, ds):
+def _specific_sort(dicom_file, dest_dir, cohort_list,
+                #    patientID, modality, 
+                ds):
     """
     Function
     ----------
@@ -108,43 +106,38 @@ def _specific_sort(dicom_file, file_path, cohort_list,
     """
     # List is append, subfolder if we add more modalities, we just
     #   update this dictionary to include the options
-    with open(os.path.join(options.base_dir, 'modality.csv'), mode='r') as csv_file:
-        input_file = csv.reader(csv_file)
-        subfolders = dict((rows[0], rows[1]) for rows in input_file)
+    # with open(os.path.join('modality.csv'), mode='r') as csv_file:
+    #     input_file = csv.reader(csv_file)
+    #     subfolders = dict((rows[0], rows[1]) for rows in input_file)
 
-    if options.alt:
-        data_dir = options.alt
-    else:
-        data_dir = os.path.join(options.base_dir, 'imported_data')
-
-    write_params = {"file_path": file_path,
-                    "patientID": patientID,
+    write_params = {"dest_dir": dest_dir,
+                    "patientID": ds.PatientID,
                     "dicom_file": dicom_file,
-                    "data_dir": data_dir,
-                    "date": None
+                    # "date": None
                     }
 
-    if hasattr(ds, 'AcquisitionDate'):
-        write_params['date'] = ds.AcquisitionDate
-    elif hasattr(ds, 'StudyDate'):
-        write_params['date'] = ds.StudyDate
-
+    # if hasattr(ds, 'AcquisitionDate'):
+        # write_params['date'] = ds.AcquisitionDate
+    # elif hasattr(ds, 'StudyDate'):
+        # write_params['date'] = ds.StudyDate
+    # print(f"{ds.PatientID}: {cohort_list}\n")
     try:
-        if int(patientID) in cohort_list or patientID in str(cohort_list):
-            if modality in subfolders.keys():
-                subfolder = subfolders[modality]
+        if int(ds.PatientID) in cohort_list or ds.PatientID in str(cohort_list):
+            # print("Found a patient")
+            if "CBCT" in ds.StudyDescription:
+                subfolder = "CBCT"
             else:
-                subfolder = ds.StudyDescription
+                subfolder = ds.Modality
             write_params.update({"subfolder": subfolder})
             _write_to_path(**write_params)
     except ValueError:
         pass
 
-
+# Command line starts below here #
+count = 0
 try:
-    csv_path = os.path.join(options.base_dir, options.csv_file)
+    csv_path = os.path.join(options.csv_file)
     cohort_list = pd.read_csv(csv_path, engine='python')['MRN'].tolist()
-    print(cohort_list)
 except FileNotFoundError:
     raise FileNotFoundError(f'The specified .csv is not at {csv_path}')
 else:
@@ -155,21 +148,27 @@ else:
             shutil.move(dicom_file,
                         os.path.join(options.base_dir, 'rejected_files'))
         else:
-            if "StudyDescription" not in dir(ds):
-                ds.add_new([0x0008, 0x1030], 'LO', '')
-            if "Modality" in dir(ds):
-                project_dir = csv_path[:-4].rpartition('/')[-1]
-                if ds.StudyDescription[:4] == 'CBCT':
-                    _specific_sort(dicom_file=dicom_file,
-                                   file_path=project_dir,
-                                   cohort_list=cohort_list,
-                                   patientID=ds.PatientID,
-                                   modality='CBCT',
-                                   ds=ds)
-                else:
-                    _specific_sort(dicom_file=dicom_file,
-                                   file_path=project_dir,
-                                   cohort_list=cohort_list,
-                                   patientID=ds.PatientID,
-                                   modality=ds.Modality,
-                                   ds=ds)
+            _specific_sort(dicom_file=dicom_file,
+                            dest_dir=options.project_dir,
+                            cohort_list=cohort_list,
+                            ds=ds)
+
+print(count)
+            # # if "StudyDescription" not in dir(ds):
+            #     # ds.add_new([0x0008, 0x1030], 'LO', '')
+            # if "Modality" in dir(ds):
+            #     # project_dir = options.project_dir
+            #     if ds.StudyDescription[:4] == 'CBCT':
+            #         _specific_sort(dicom_file=dicom_file,
+            #                        file_path=project_dir,
+            #                        cohort_list=cohort_list,
+            #                     #    patientID=ds.PatientID,
+            #                        modality='CBCT',
+            #                        ds=ds)
+            #     else:
+            #         _specific_sort(dicom_file=dicom_file,
+            #                        file_path=project_dir,
+            #                        cohort_list=cohort_list,
+            #                     #    patientID=ds.PatientID,
+            #                        modality=ds.Modality,
+            #                        ds=ds)
