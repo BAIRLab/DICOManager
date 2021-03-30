53ceb435e6dcf951f147dbe270eb662491eb8bb0
Evan Porter <evmikeport@gmail.com>
2021-03-30 17:01:14 -0400

    functional rtstruct reconstruction at frame of ref level

--- processing.py
+++ processing.py
@@ -2,43 +2,7 @@ import cv2
 import numpy as np
 import pydicom
 from scipy.interpolate import RegularGridInterpolator as RGI
-from dataclasses import dataclass, fields
-
-
-@dataclass
-class ImageVolume:
-    # A single volume, single modality reconstruction
-    # This would be returned in the following:
-    # series = Series(files)
-    # ct_vol = series.reconstruct.ct()
-    # struct = series.reconstruct.struct()
-    array: np.array
-    dicom_header: pydicom.dataset.Dataset
-    dimension: list = None
-    pixelspacing: list = None
-    slicethickness: float = None
-    interpolated: bool = False
-    missing_slices: list = None
-
-    def __post_int__(self):
-        self.pixelspacing = self.dicom_header.PixelSpacing
-        self.slicethickness = self.dicom_header.SliceThickness
-        self.dicom_header = None
-
-
-@dataclass
-class StructVolumeSet:
-    volumes: dict  # Named as the structures and the volumes
-    dimension: list = None
-    pixelspacing: list = None
-    slicethickness: float = None
-    interpoltaed: bool = False
-    missing_slices: list = None
-
-    def __post_int__(self):
-        self.pixelspacing = self.dicom_header.PixelSpacing
-        self.slicethickness = self.dicom_header.SliceThickness
-        self.dicom_header = None
+from dataclasses import dataclass, field, fields
 
 
 @dataclass
@@ -52,6 +16,7 @@ class VolumeDimensions:
     dx: float = None
     dy: float = None
     dz: float = None
+    vox_size: list = None
     flipped: bool = False
     multi_thick: bool = False
 
@@ -75,6 +40,7 @@ class VolumeDimensions:
         self.dx, self.dy = map(float, ds.PixelSpacing)
         self.dims = [self.rows, self.cols, self.slices]
         self.position = ds.PatientPosition
+        self.vox_size = [self.dx, self.dy, self.dz]
         self.dicoms = None
 
     def _calc_n_slices(self, files: list):
@@ -116,7 +82,7 @@ class VolumeDimensions:
 
         self.dz = min(slice_thicknesses)
         self.origin = np.array([*ipp[:2], min(z0, z1)])
-        self.slices = round(max(z0, z1) - min(z0, z1)) / self.dz
+        self.slices = round((max(z0, z1) - min(z0, z1)) / self.dz)
 
         if z1 > z0:
             # TODO: We can replace thiw with the ImagePositionPatient header
@@ -142,15 +108,125 @@ class VolumeDimensions:
         return grid.reshape(3, -1)
 
 
+@dataclass
+class StructVolumeSet:
+    dims: VolumeDimensions
+    volumes: dict = field(default_factory=dict)  # Named as the structures and the volumes
+    interpoltaed: bool = False
+    missing_slices: list = None
+
+    def __getitem__(self, name: str):
+        return self.volumes[name]
+
+    def __setitem__(self, name: str, volume: np.array):
+        if name in self.volumes:
+            name = self._rename(name)
+        self.volumes.update({name: volume})
+
+    def _rename(self, name: str):
+        """[DICOM RTSTRUCTs can have non-unique names, so we need to rename
+            these functions to be dictionary compatiable]
+
+        Args:
+            name (str): [Name of the found RTSTRUCT]
+
+        Returns:
+            [str]: [Name + # where number is the unique occurance]
+        """
+        i = 0
+        while True:
+            temp = name + i
+            if temp not in self.volumes:
+                break
+            i += 1
+        return temp
+
+    @property
+    def shape(self):
+        return (len(self.volumes), *self.dims.shape)
+
+    @property
+    def names(self):
+        self.volumes = dict(sorted(self.volumes.items()))
+        return list(self.volumes.keys())
+
+
+@dataclass
+class ImageVolume:
+    # A single volume, single modality reconstruction
+    # This would be returned in the following:
+    # series = Series(files)
+    # ct_vol = series.reconstruct.ct()
+    # struct = series.reconstruct.struct()
+    dims: VolumeDimensions
+    array: np.array
+    dicom_header: pydicom.dataset.Dataset
+    interpolated: bool = False
+    missing_slices: list = None
+
+
 class Reconstruction:
     def __call__(self, frame_of_ref):
-        self._create_dims(frame_of_ref)
+        self.define_vol_dims(frame_of_ref)
         for mod in frame_of_ref.iter_modalities:
-            print(len(mod.dicoms))
-        print(self.dims)
-        print()
+            # TODO: Make switch statement in python 3.10
+            """
+            if mod.dirname == 'CT':
+                vol = self.ct(mod)
+            elif mod.dirname == 'MR':
+                vol = self.mr(mod)
+            elif mod.dirname == 'PET':
+                vol = self.pet(mod)
+            elif mod.dirname == 'DOSE':
+                vol = self.dose(mod)
+            """
+            if mod.dirname == 'RTSTRUCT':
+                vols = self.struct(mod)
+                for vol in vols:
+                    print(vol.shape, vol.names)
 
-    def _create_dims(self, frame_of_ref):
+    def _slice_coords(self, contour_slice: pydicom.dataset.Dataset) -> (np.array, int):
+        """[converts a dicom contour slice into image coordinates]
+
+        Args:
+            contour_slice (pydicom.dataset.Dataset): [(3006, 0016): Contour Image Sequence]
+
+        Returns:
+            [np.array]: [2D numpy array of contour points in image coordinates]
+            [int]: [The z location of the rtstruct slice location]
+        """
+        contour_pts = np.array(contour_slice.ContourData).reshape(-1, 3)
+        pts_diff = abs(contour_pts - self.dims.origin)
+        points = np.array(np.round(pts_diff / self.dims.vox_size), dtype=np.int32)
+        coords = np.array([points[:, :2]], dtype=np.int32)
+        zloc = list(set(points[:, 2]))
+        if len(zloc) > 1:
+            raise ValueError('RTSTRUCT not registered to rectilinear coordinates')
+        return (coords, zloc[0])
+
+    def _build_contour(self, contour_data):
+        fill_array = np.zeros(self.dims.shape)
+        for contour_slice in contour_data:
+            coords, zloc = self._slice_coords(contour_slice)
+
+            poly2D = np.zeros(self.dims.shape[:2])
+            cv2.fillPoly(poly2D, coords, 1)
+
+            fill_array[:, :, zloc] += poly2D
+        fill_array = fill_array % 2
+        return fill_array
+
+    def check_dims(self):
+        def _check_dims(func):
+            def wrapped(self, modality, *args, **kwargs):
+                if not hasattr(self, 'dims'):
+                    if modality.name in ['CT', 'MR']:
+                        self.dims = VolumeDimensions(modality.data)
+                func(*args, **kwargs)
+            return wrapped
+        return _check_dims
+
+    def define_vol_dims(self, frame_of_ref):
         if not hasattr(self, 'dims'):
             for mod in frame_of_ref.iter_modalities:
                 if mod.name in ['CT', 'MR']:
@@ -159,65 +235,47 @@ class Reconstruction:
             if not hasattr(self, 'dims'):
                 raise TypeError('Volume dimensions not created, no MR or CT in Frame Of Reference')
 
-    def _point_to_coords(self, contour_pts, ct_group):
-        img_origin = ct_group.origin
-        vox_size = [*ct_group.PixelSpacing, abs(ct_group.SliceThickness)]
-        diff = abs(contour_pts - img_origin)
-        points = np.array(np.round(diff / vox_size), dtype=np.int32)
-        return points
-
-    def _build_contour(self, contour_data, ct_group):
-        fill_array = np.zeros(ct_group.dims)
-        for contour_slice in contour_data:
-            contour_pts = np.array(contour_slice.ContourData).reshape(-1, 3)
-
-            points = self._points_to_coords(contour_pts, ct_group)
-            coords = np.array([points[:, :2]], dtype=np.int32)
-
-            poly2D = np.zeros(ct_group.dims[:2])
-            cv2.fillPoly(poly2D, coords, 1)
-
-            fill_array[:, :, points[0, 2]] += poly2D
-        fill_array = fill_array % 2
-        return fill_array
-
-    def struct(self, series):
+    def struct(self, modality):
+        if not hasattr(self, 'dims'):
+            raise LookupError('Must define volume dimensions first')
         # Return as a struct volume type
         struct_sets = []
-        for struct_group in series.struct:
-            struct_set = StructVolumeSet()
-            ref_group = series.frame_of_ref_subset(struct_group.FrameOfRef)
-            ct_group = ref_group.ct[0]
-            for struct_file in struct_group:
-                ds = pydicom.dcmread(struct_file)
+        for struct_group in modality.struct:
+            struct_set = StructVolumeSet(self.dims)
+            for struct in struct_group:
+                ds = pydicom.dcmread(struct.filepath)
                 for index, contour in enumerate(ds.StructureSetROISequence):
                     name = contour.ROIName.lower()
                     contour_data = ds.ROIContourSequence[index].ContourSequence
-                    built = self._build_contour(contour_data, ct_group)
-                    struct_set.add(built, name)
+                    built = self._build_contour(contour_data)
+                    struct_set[name] = built
             struct_sets.append(struct_set)
         return struct_sets
 
-    def ct(self, series, HU=True):
+    @check_dims
+    def ct(self, modality, HU=True):
         ct_vols = []
-        for ct_group in series.ct:
-            fill_array = np.zeros(ct_group.dims, dtype='float32')
-            origin = ct_group.origin
+        for ct_group in modality.ct:
+            fill_array = np.zeros(self.dims.shape, dtype='float32')
+            origin = self.dims.origin
 
             for ct_file in ct_group:
                 ds = pydicom.dcmread(ct_file)
                 z_loc = int(round(abs(origin[-1] - ds.SliceLocation)))
                 fill_array[:, :, z_loc] = ds.pixel_array
 
+            slope = ds.RescaleSlope
+            intercept = ds.RescaleIntercept
             if HU:
-                ct_vols.append(fill_array * ds.RescaleSlope +
-                               ds.RescaleIntercept)
-            ct_vols.append(fill_array)
+                ct_vols.append(fill_array * slope + intercept)
+            else:
+                ct_vols.append(fill_array)
         return ct_vols
 
-    def nm(self, series):
+    @check_dims
+    def nm(self, modality):
         nm_vols = []
-        for nm_group in series.nm:
+        for nm_group in modality.nm:
             ds = pydicom.dcmread(nm_group[0])
             raw = np.rollaxis(ds.pixel_array, 0, 3)[:, :, ::-1]
             map_seq = ds.RealWorldValueMappingSequence[0]
@@ -226,9 +284,10 @@ class Reconstruction:
             nm_vols.append(raw * slope + intercept)
         return nm_vols
 
-    def mr(self, series):
+    @check_dims
+    def mr(self, modality):
         mr_vols = []
-        for mr_group in series.mr:
+        for mr_group in modality.mr:
             fill_array = np.zeros(mr_group.dims)
             origin = mr_group.origin
 
@@ -239,9 +298,10 @@ class Reconstruction:
             mr_vols.append(fill_array)
         return mr_vols
 
-    def pet(self, series):
+    @check_dims
+    def pet(self, modality):
         pet_vols = []
-        for pet_group in series.pet:
+        for pet_group in modality.pet:
             fill_array = np.zeros(pet_group.dims)
             origin = pet_group.origin
 
@@ -257,18 +317,18 @@ class Reconstruction:
             pet_vols.append(rescaled * patient_weight / total_dose)
         return pet_vols
 
-    def dose(self, series):
+    @check_dims
+    def dose(self, modality):
         dose_vols = {}
-        ct_dims = VolumeDimensions(series.ct)
-        ct_coords = ct_dims.coordgrid()
-        for dosefile in series.dose:
+        ct_coords = self.dims.coordgrid()
+        for dosefile in modality.dose:
             ds = pydicom.dcmread(dosefile.filepath)
             dose_dims = VolumeDimensions(dosefile)
-            dose_array = np.rollaxis(ds.pixel_array, 0, 3) * ds.DoseGridScaling
             dose_coords = dose_dims.coordrange()
+            dose_array = np.rollaxis(ds.pixel_array, 0, 3) * ds.DoseGridScaling
             interper = RGI(dose_coords, dose_array,
                            bounds_error=False, fill_value=0)
-            dose_interp = interper(ct_coords).reshape(ct_dims.shape)
+            dose_interp = interper(ct_coords).reshape(self.dims.shape)
             dose_vols.update({dosefile: dose_interp})
         return dose_vols
 
