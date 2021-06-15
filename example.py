a0de8b29c775176bded79258dc37e16ae3d35ee4
Evan Porter <evmikeport@gmail.com>
2021-06-15 12:00:40 -0400

    expanded example to include filtering

--- example.py
+++ example.py
@@ -1,21 +1,26 @@
 from groupings import Cohort
 from glob import glob
-import utils
 import time
 import sys
 
 '''
+# in the format of:
 filter_list = {'PatientID': [...],
                'StudyDate': [...],
                'SeriesDate': [...],
                'StructName': [...]}
 '''
+filter_list = {'StructName': {'hippocampus': ['hippocampus'],
+                              'hippo_avoid': ['hippoavoid', 'hippo_avoid']},
+               'Modality': ['CT', 'RTSTRUCT']}
 
 start = time.time()
-files = glob('/home/eporter/eporter_data/hippo_data_fldr/hippo_data/*/**/*.dcm', recursive=True)
-cohort = Cohort(name='TestFileSave', files=files, include_series=False)
-cohort.recon(parallelize=True, in_memory=False)
+files = glob('/home/eporter/eporter_data/rtog_project/MIMExport/**/*.dcm', recursive=True)
+cohort = Cohort(name='RTOG_Hippocampus', files=files, include_series=False, filter_by=filter_list)
 print(cohort)
+#cohort.save_tree(path='/home/eporter/eporter_data/rtog_project/dicoms/')
+print('tree saved, now reconstructing')
+cohort.recon(parallelize=True, in_memory=False, path='/home/eporter/eporter_data/rtog_project/built/')
 print('elapsed:', time.time() - start)
 print(len(cohort))
 sys.stdout.flush()
