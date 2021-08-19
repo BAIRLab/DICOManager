56d512cf82abe80bae753353c23c8dee297bdaf0
Evan Porter <evmikeport@gmail.com>
2021-08-19 15:19:55 -0400

    Modifications to accessors

--- xarray_playground.py
+++ xarray_playground.py
@@ -1,6 +1,8 @@
 import xarray as xr
 import numpy as np
 import pydicom
+from dataclasses import dataclass, field
+from glob import glob
 
 # Can either create data array with function, or feed the dcm_header and volume
 # into data array and use the backend accessor to compute those things.
@@ -17,44 +19,37 @@ class VolumeDimensions:
     dx: float = None
     dy: float = None
     dz: float = None
+    voxel_size: list = None
     ipp: list = None
     flipped: bool = False
     multi_thick: bool = False
     # If I want to get fancy I could make the fields below dynamic, reading from another
     # list of fields, but that is poor form
     PatientID: set = field(default_factory=set)
-    Modality: set = field(defualt_factory=set)
+    Modality: set = field(default_factory=set)
     SOPInstanceUID: set = field(default_factory=set)
-    StudyInstanceUID: set = field(defualt_factory=set)
-    SeriesInstanceUID: set = field(defualt_factory=set)
-    FrameOfReferenceUID: set = field(defualt_factory=set)
-    SeriesDescription: set = field(defualt_factory=set)
-    StudyDescription: set = field(defualt_factory=set)
+    StudyInstanceUID: set = field(default_factory=set)
+    SeriesInstanceUID: set = field(default_factory=set)
+    FrameOfReferenceUID: set = field(default_factory=set)
+    SeriesDescription: set = field(default_factory=set)
+    StudyDescription: set = field(default_factory=set)
 
     def __post_init__(self):
         # TODO: INTEGRATE DATETIME
         # TODO: ACCOUNT FOR THIS!
-        if 'RTDOSE' in self.dicoms:
-            filepath = self.dicoms['RTDOSE'][0].filepath
-            ds = pydicom.dcmread(filepath)
+        ds = self._calc_n_slices(self.dicoms)
+        if ds.Modality == 'RTDOSE':
             self.slices = ds.NumberOfFrames
             self.origin = ds.ImagePositionPatient
             self.dz = float(ds.SliceThickness)
-        else:
-            if 'CT' in self.dicoms:
-                files = self.dicoms['CT']
-            elif 'MR' in self.dicoms:
-                files = self.dicoms['MR']
-
-            ds = self._calc_n_slices(files)
 
         self.rows = ds.Rows
         self.cols = ds.Columns
         self.dx, self.dy = map(float, ds.PixelSpacing)
+        self.voxel_size = np.array([self.dx, self.dy, self.dz], dtype=np.half)
         self.position = ds.PatientPosition
-        self.dicoms = None
 
-    def _pull_header_info(self, dicom):
+    def _pull_header_info(self, ds):
         for field in self.__dataclass_fields__:
             if field == 'FrameOfReferenceUID':
                 if hasattr(ds, 'FrameOfReferenceUID'):
@@ -65,8 +60,8 @@ class VolumeDimensions:
                 else:
                     frame = None
                 self.FrameOfReferenceUID = frame
-            elif hasattr(dicom, field):
-                value = getattr(dicom, field)
+            elif hasattr(ds, field):
+                value = getattr(ds, field)
                 setattr(self, field, value)
 
     def _calculate_dz(self, zlocations):
@@ -104,7 +99,9 @@ class VolumeDimensions:
         zlocations = []
 
         for dcm in files:
-            ds = pydicom.dcmread(dcm.filepath, stop_before_pixels=True)
+            ds = pydicom.dcmread(dcm, stop_before_pixels=True)
+            if ds.Modality == 'RTDOSE':
+                return ds
             self._pull_header_info(ds)
             self.ipp = ds.ImagePositionPatient
             self.iop = ds.ImageOrientationPatient
@@ -137,19 +134,19 @@ class VolumeDimensions:
         return ds
 
     def report_coords(self):
-        shape = [self.rows, self.cols, self.slices]
-        voxel_size = [self.dx, self.dy, self.dz]
-        lo = self.origin
-        hi = self.origin + voxel_size * shape
-        output = {'x_ipp': np.linspace(lo[0], hi[0], self.dx),
-                  'y_ipp': np.linspace(lo[1], hi[1], self.dy),
-                  'z_ipp': np.linspace(lo[2], hi[2], self.dz)
+        output = {'x_ipp': self.origin[0] + np.arange(self.rows) * self.dx,
+                  'y_ipp': self.origin[1] + np.arange(self.cols) * self.dy,
+                  'z_ipp': self.origin[2] - np.arange(self.slices) * self.dz
                   }
+        for key, value in output.items():
+            output[key] = np.array(value, dtype=np.half)
         return output
 
     def report_attrs(self):
         output = {}
         for field in self.__dataclass_fields__:
+            if field == 'dicoms':
+                continue
             value = getattr(self, field)
             if type(value) is set:
                 value = list(value)
@@ -157,38 +154,35 @@ class VolumeDimensions:
         return output
 
 
-def create_dataarray(volume, dicom_files):
-    volume_dimensions = VolumeDimensions(dicom_files)
-    attrs = volume_dimensions.report_attrs()
-    coords = volume_dimensions.report_coords()
 
-    new_da = xr.DataArray(volume, coords=coords, dims=['x_ipp', 'y_ipp', 'z_ipp'])
-    new_da.attrs = attrs
-
-    return new_da
-
-
-@xr.registered_dataset_accessor("dims")
-class ImageDims:
+@xr.register_dataarray_accessor("is_struct")
+class IsStruct:
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
-        self._obj.attr
 
-    @property
-    def field_of_view(self):
-        shape = np.array(self.shape)
-        voxel_size = np.array(self.voxel_size)
+    def __call__(self):
+        return self._obj.Modality == 'RTSTRUCT'
+
+
+@xr.register_dataarray_accessor("field_of_view")
+class VolumeShape:
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def __call__(self):
+        shape = np.array(self._obj.shape)
+        voxel_size = np.array(self._obj.voxel_size)
+        if self._obj.is_struct:
+            return shape[1:] * voxel_size
         return shape * voxel_size
 
-    @property
-    def shape(self):
-        return [self._obj.rows, self._obj.columns, self._obj.slices]
 
-    @property
-    def voxel_size(self):
-        return [self._obj.dx, self._obj.dy, self._obj.dz]
+@xr.register_dataarray_accessor("coordrange")
+class CoordRange:
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
 
-    def coordrange(self):
+    def __call__(self):
         pts_x = self._obj.origin[0] + np.arange(self._obj.rows) * self._obj.dx
         pts_y = self._obj.origin[1] + np.arange(self._obj.cols) * self._obj.dy
         pts_z = self._obj.origin[2] + np.arange(self._obj.slices) * self._obj.dz
@@ -196,30 +190,42 @@ class ImageDims:
             pts_z = pts_z[..., ::-1]
         return [pts_x, pts_y, pts_z]
 
-    def coordgrid(self):
-        pts_x, pts_y, pts_z = self.coordrange()
+
+@xr.register_dataarray_accessor("coordgrid")
+class CoordGrid:
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def __call__(self):
+        pts_x, pts_y, pts_z = self._obj.coordrange()
         grid = np.array([*np.meshgrid(pts_x, pts_y, pts_z, indexing='ij')])
         return grid.reshape(3, -1)
 
-    def calc_z_index(self, loc):
+
+@xr.register_dataarray_accessor("calc_z_index")
+class CalcZIndex:
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def __call__(self):
         return int(round(abs((self._obj.origin[-1] - loc) / self._obj.dz)))
 
-    def Mgrid(self, dcm_hdr):
-        """
-        Function
-        ----------
-        Given a DICOM CT image slice, returns an array of pixel coordinates
 
-        Returns
-        ----------
-        numpy.ndarray
-            A numpy array of shape Mx2 where M is the dcm.Rows x dcm.Cols,
-            the number of (x, y) pairs representing coordinates of each pixel
+@xr.register_dataarray_accessor("mgrid")
+class MGrid:
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
 
-        Notes
-        ----------
-        Computes M via DICOM Standard Equation C.7.6.2.1-1
-            https://dicom.innolitics.com/ciods/ct-image/image-plane/00200037
+    def __call__(self):
+        """Given a DICOM CT image slice, returns an array of pixel coordinates
+           for the given frame of reference. Useful for converting boolean masks
+           to RTSTRUCT files
+
+        Args:
+            dcm_hdr (pydicom.dataset.Dataset): pydicom dataset object of DICOM header
+
+        Returns:
+            np.ndarray: A numpy array of the patient position coordinates
         """
         IOP = dcm_hdr.ImageOrientationPatient
         IPP = dcm_hdr.ImagePositionPatient
@@ -239,12 +245,12 @@ class ImageDims:
         return np.rollaxis(np.stack(np.matmul(M, C)), 0, 3)
 
 
-@xr.registered_dataset_accessor("_resample_update")
-class ResampledUpdate:
+@xr.register_dataarray_accessor("_resample_update")
+class ResampledUpdateArray:
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
 
-    def __call__(self, ratio: flaot):
+    def __call__(self, ratio: float):
         if type(ratio) is int or type(ratio) is float:
             ratio = np.array([ratio, ratio, ratio])
         if type(ratio) is list:
@@ -259,8 +265,8 @@ class ResampledUpdate:
         self._obj.slices = int(round(self.slices * ratio[2]))
 
 
-@xr.registared_dataset_accessor("_crop_update")
-class CropUpdate:
+@xr.register_dataarray_accessor("_crop_update")
+class CropUpdateArray:
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
 
@@ -271,7 +277,40 @@ class CropUpdate:
         self._obj.slices = zhi - zlo
 
 
-@xr.registered_dataset_accessor("io")
+@xr.register_dataset_accessor('iter_modalities')
+class IterModalities:
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def __call__(self):
+        for mod in ds.data_vars:
+            yield mod
+
+
+@xr.register_dataset_accessor("_resample_update")
+class ResampledUpdateSet:
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def __call__(self, ratio: float):
+        for mod in self._obj.iter_modalities():
+            mod._resample_update(ratio)
+
+
+@xr.register_dataset_accessor("_crop_update")
+class CropUpdateSet:
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def __call__(self, values: list):
+        for mod in self._obj.iter_modalities():
+            mod._crop_update(values)
+
+
+# We could make each of theses methods standalone functions, but I'm
+# afraid that basic ones like 'save' and 'load' will interfere or overload
+# the default xarray functions. Therefore I've offset them in the io category
+@xr.register_dataset_accessor("io")
 class ReconstructedIO:
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
@@ -301,7 +340,7 @@ class ReconstructedIO:
     def is_pointer(self):
         return self._is_pointer()
 
-    def save(self, filepath=None):
+    def save_array(self, filepath=None):
         if not self.is_pointer():
             if filepath:
                 self._filepath = filepath
@@ -326,42 +365,50 @@ class ReconstructedIO:
             self._is_pointer = True
 
 
-@xr.register_dataset_accessor("struct")
-class StructureAccessor:
-    def __init__(self, xarray_obj):
-        self._obj = xarray_obj
-
-    def __getattr__(self, name):
-        if name not in self._obj.struct_names:
-            return None
-        index = self._obj.struct_names.index(name)
-        return self._obj.structs[index]
-
-    def as_dict(self):
-        if self._obj['struct']:
-            output = {}
-            for name, vol in zip(self._obj['struct'], self._obj.struct_names):
-                output.update({name: vol})
-            return output
-        return None
+def create_dataset(Modality):
+    dims = {}
+    for mod, dicoms in Modality.dicom_files:
+        dims.update({mod: create_dataarray(dicoms)})
+    new_ds = xr.Dataset(dims)
+    Modality.volume_data = new_ds
 
 
-@xr.register_dataset_accessor("ct")
-class CTAccessor:
-    def __init__(self, xarray_obj):
-        self._obj = xarray_obj
+def create_dataarray(volume, dicom_files, struct_names=None):
+    volume_dimensions = VolumeDimensions(dicom_files)
+    attrs = volume_dimensions.report_attrs()
+    coords = volume_dimensions.report_coords()
+    name = volume_dimensions.SeriesInstanceUID
+
+    dims = ['x_ipp', 'y_ipp', 'z_ipp']
+    if struct_names is not None:
+        dims = ['struct_names'] + dims
+        new_coords = ({'struct_names': struct_names})
+        new_coords.update(coords)
+        coords = new_coords
+
+    new_da = xr.DataArray(volume, name=name, coords=coords)#, dims=dims)
+    new_da.attrs = attrs
+
+    return new_da
 
 
-@xr.register_dataset_accessor("mr")
-class MRAccessor:
-    def __init__(self, xarray_obj):
-        self._obj = xarray_obj
+# Each image will be a xarray.DataArray
+# The dims will simply be the x, y, z coordinates
+# For a structure, there will also be structure names
+# structure will have a hidden accessor for is_structure
+# Then the overall reconstructed group will be stored as a
+# xarray.Dataset with the same coordinates for x, y, z
+# and a series of special methods to implement special group
+# funciontality
 
-
-# Each volume will be stored as a DataArray
-# CT, MR, NM, RTDOSE will be [X, Y, Z]
-# RTSTRUCT will be [N, X, Y, Z] where N is structure name
-
-class ReconstructedSeries:
-    def __init__(self, xarray_obj):
-        self._obj = xarray_obj
+if __name__ == '__main__':
+    ct_files = glob('/home/eporter/eporter_data/rtog_project/dicoms/train/**/Patient_10*71/**/CT/*.dcm', recursive=True)
+    volume = np.zeros((5, 512, 512, len(ct_files)))
+    print(volume.shape)
+    struct_names = ['rt' + str(n) for n in range(5)]
+    xrda = create_dataarray(volume=volume, dicom_files=ct_files, struct_names=struct_names)
+    xrds = xrda.to_dataset()
+    print(xrda)
+    print(xrda.attrs)
+    print(xrda.field_of_view())
+    print(xrds)
